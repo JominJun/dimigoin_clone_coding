f04c1688bd8849a2731b547470c13954c95b8e08
jominjun <mj3029327@naver.com>
2020-09-30 21:42:45 +0900

    메인 페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,106 +1,15 @@
-import React, { useState } from "react";
-import axios from "axios";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import "./App.css";
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { Home } from "../src/pages";
 
-const App = () => {
-  const [status, setStatus] = useState({
-    isLogin: false,
-    isLoading: false,
-    isError: false,
-    errorStatusCode: "",
-  });
-
-  const [userInfo, setUserInfo] = useState({
-    id: "",
-    pw: "",
-    accessToken: "",
-    refreshToken: "",
-  });
-
-  const handleChange = (event) => {
-    setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
-  };
-
-  const prelogin = () => {
-    // 로딩 중이 아니며 로그인하지 않았다면
-    if (!status.isLoading && !status.isLogin && !status.isError) {
-      setStatus({ ...status, isLoading: true });
-      login();
-    }
-  };
-
-  const login = async () => {
-    await axios
-      .post("https://api.dimigo.in/auth/", {
-        id: userInfo.id,
-        password: userInfo.pw,
-      })
-      .then((response) => {
-        setStatus({
-          ...status,
-          isLogin: true,
-          isLoading: false,
-        });
-        setUserInfo({
-          accessToken: response.data.token,
-          refreshToken: response.data.refresh_token,
-        });
-      })
-      .catch((error) => {
-        setStatus({
-          ...status,
-          isError: true,
-          errorStatusCode: error.response.status,
-          isLoading: false,
-        });
-      });
-  };
-
-  if (status.isLoading) {
+class App extends Component {
+  render() {
     return (
-      <>
-        <CircularProgress color="secondary" />
-      </>
+      <div>
+        <Route exact path="/" component={Home} />
+      </div>
     );
   }
-
-  if (status.isError) {
-    if (status.errorStatusCode === 404) {
-      return <>아이디 또는 패스워드가 일치하지 않습니다</>;
-    }
-    return <>Error: {status.errorStatusCode}</>;
-  }
-
-  if (status.isLogin) {
-    return (
-      <>
-        로그인 함<br />
-        <b>accessToken</b>: {userInfo.accessToken} <br />
-        <b>refreshToken</b>: {userInfo.refreshToken}
-      </>
-    );
-  }
-
-  return (
-    <>
-      <form name="LoginForm" onSubmit={prelogin}>
-        <input
-          name="id"
-          type="text"
-          placeholder="아이디를 입력하세요"
-          onChange={handleChange}
-        />
-        <input
-          name="pw"
-          type="password"
-          placeholder="비밀번호를 입력하세요"
-          onChange={handleChange}
-        />
-        <button type="submit">로그인</button>
-      </form>
-    </>
-  );
-};
+}
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,191 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import "./Home.css";
+
+const Home = () => {
+  const [status, setStatus] = useState({
+    isLogin: false,
+    isLoading: false,
+    isError: false,
+    errorStatusCode: "",
+  });
+
+  const [userInfo, setUserInfo] = useState({
+    id: "",
+    pw: "",
+    accessToken: "",
+    refreshToken: "",
+  });
+
+  const [mealInfo, setMealInfo] = useState({
+    breakfast: "",
+    lunch: "",
+    dinner: "",
+    isError: false,
+    errorStatusCode: "",
+  });
+
+  useEffect((mealInfo) => {
+    axios
+      .get("https://api.dimigo.in/dimibobs/today/")
+      .then((response) => {
+        setMealInfo({
+          ...mealInfo,
+          breakfast: response.data.breakfast,
+          lunch: response.data.lunch,
+          dinner: response.data.dinner,
+        });
+      })
+      .catch((error) => {
+        console.log("에러 발생");
+        setMealInfo({
+          ...mealInfo,
+          isError: true,
+          errorStatusCode: error.response.status,
+        });
+      });
+  }, []);
+
+  const handleChange = (event) => {
+    setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
+  };
+
+  const prelogin = () => {
+    // 로딩 중이 아니며 로그인하지 않았다면
+    if (!status.isLoading && !status.isLogin && !status.isError) {
+      setStatus({ ...status, isLoading: true });
+      login();
+    }
+  };
+
+  const login = async () => {
+    await axios
+      .post("https://api.dimigo.in/auth/", {
+        id: userInfo.id,
+        password: userInfo.pw,
+      })
+      .then((response) => {
+        setStatus({
+          ...status,
+          isLogin: true,
+          isLoading: false,
+        });
+        setUserInfo({
+          accessToken: response.data.token,
+          refreshToken: response.data.refresh_token,
+        });
+      })
+      .catch((error) => {
+        setStatus({
+          ...status,
+          isError: true,
+          errorStatusCode: error.response.status,
+          isLoading: false,
+        });
+      });
+  };
+
+  if (status.isLoading) {
+    return (
+      <>
+        <CircularProgress color="secondary" />
+      </>
+    );
+  }
+
+  if (status.isError) {
+    if (status.errorStatusCode === 404) {
+      return <>아이디 또는 패스워드가 일치하지 않습니다</>;
+    }
+    return <>Error: {status.errorStatusCode}</>;
+  }
+
+  if (status.isLogin) {
+    return (
+      <>
+        로그인 함<br />
+        <b>accessToken</b>: {userInfo.accessToken} <br />
+        <b>refreshToken</b>: {userInfo.refreshToken}
+      </>
+    );
+  }
+
+  return (
+    <div id="wrapBox">
+      <div id="loginBox">
+        <div id="logoBox">
+          <svg
+            id="logo"
+            data-v-5674a119=""
+            xmlns="http://www.w3.org/2000/svg"
+            data-name="Layer 1"
+            viewBox="0 0 128 33"
+          >
+            <path
+              data-v-5674a119=""
+              d="M11 21.13H7.85A7.85 7.85 0 012.3 7.73a2.41 2.41 0 013.41 3.41 3 3 0 002.14 5.16h.77V2.41a2.42 2.42 0 014.83 0v16.3A2.42 2.42 0 0111 21.13zm10.6-2.42v-7.24a2.42 2.42 0 10-4.83 0v7.24a2.42 2.42 0 104.83 0zm-2-10.91a2.44 2.44 0 00.46-.14 3.23 3.23 0 00.41-.22 2.2 2.2 0 00.37-.3 2.69 2.69 0 00.3-.37 2.1 2.1 0 00.22-.42 2.35 2.35 0 00.14-.45 2.42 2.42 0 00.05-.47 2.45 2.45 0 00-.71-1.71 2.2 2.2 0 00-.37-.3 2.32 2.32 0 00-.41-.22 2.44 2.44 0 00-.46-.14 2.48 2.48 0 00-.94 0 2.35 2.35 0 00-.45.14 2.1 2.1 0 00-.42.22 3.47 3.47 0 00-.37.3 2.63 2.63 0 00-.29.37 2.16 2.16 0 00-.36.87 1.83 1.83 0 00-.05.47 1.83 1.83 0 00.05.47 2.32 2.32 0 00.13.45 2.13 2.13 0 00.23.42 2.06 2.06 0 00.29.37 2.5 2.5 0 002.18.66zm29.54-.24a7.86 7.86 0 00-11-.1 7.84 7.84 0 00-13.28 5.65V19a2.42 2.42 0 104.83 0v-5.9a3 3 0 016 0V19a2.42 2.42 0 104.83 0v-5.9a3 3 0 116 0V19a2.42 2.42 0 004.84 0v-5.9a7.83 7.83 0 00-2.17-5.54zm10.5 11.15v-7.24a2.42 2.42 0 10-4.83 0v7.24a2.42 2.42 0 104.83 0zm-2-10.91a2.15 2.15 0 00.45-.14 2.78 2.78 0 00.42-.22 2.69 2.69 0 00.37-.3 2.69 2.69 0 00.3-.37 2.1 2.1 0 00.22-.42 2.35 2.35 0 00.14-.45 2.23 2.23 0 000-.94 2.15 2.15 0 00-.14-.45 2.1 2.1 0 00-.22-.42 2.69 2.69 0 00-.3-.37 2.69 2.69 0 00-.37-.3 2.1 2.1 0 00-.42-.22 2.15 2.15 0 00-.45-.14 2.48 2.48 0 00-.94 0 2.35 2.35 0 00-.45.14 2.1 2.1 0 00-.42.22 2.69 2.69 0 00-.37.3 2.69 2.69 0 00-.3.37 2.78 2.78 0 00-.22.42 2.15 2.15 0 00-.14.45 2.5 2.5 0 00.66 2.18 2.69 2.69 0 00.37.3 2.78 2.78 0 00.42.22 2.35 2.35 0 00.45.14 2.78 2.78 0 00.94 0zm19.76 6.08v10.87a7.25 7.25 0 11-14.49 0 2.42 2.42 0 114.83 0 2.42 2.42 0 104.83 0V20.7a7.24 7.24 0 114.83-6.82zm-4.83 0a2.42 2.42 0 10-2.37 2.42 2.42 2.42 0 002.42-2.42zm22.34 0a7.25 7.25 0 10-7.25 7.25A7.25 7.25 0 0095 13.88zm-4.83 0a2.42 2.42 0 11-2.42-2.41 2.43 2.43 0 012.47 2.41z"
+            ></path>
+            <path
+              data-v-5674a119=""
+              d="M103.41 20.67a2.42 2.42 0 01-2.41-2.41V11a2.42 2.42 0 014.83 0v7.25a2.41 2.41 0 01-2.42 2.42zm.47-13.33a2.32 2.32 0 00.45-.13 2.13 2.13 0 00.42-.23 1.74 1.74 0 00.37-.3 2.6 2.6 0 00.3-.36 2.78 2.78 0 00.22-.42 2.35 2.35 0 00.14-.45 2.56 2.56 0 000-.47 2.42 2.42 0 000-.47 2.44 2.44 0 00-.14-.46 3.23 3.23 0 00-.22-.41 2.2 2.2 0 00-.3-.37 2.69 2.69 0 00-.37-.3 2.1 2.1 0 00-.42-.22 1.81 1.81 0 00-.45-.14 2.48 2.48 0 00-.94 0 1.69 1.69 0 00-.45.14 2.1 2.1 0 00-.42.22 2.69 2.69 0 00-.37.3 2.2 2.2 0 00-.3.37 2.32 2.32 0 00-.22.41 2.44 2.44 0 00-.14.46A2.42 2.42 0 00101 5a2.59 2.59 0 00.05.47 2.35 2.35 0 00.14.45 2.1 2.1 0 00.22.42 2.6 2.6 0 00.3.36 1.74 1.74 0 00.37.3 2.16 2.16 0 00.87.36 2.23 2.23 0 00.94 0zm21 11.07v-5.9a7.85 7.85 0 10-15.69 0v5.9a2.42 2.42 0 104.83 0v-5.9a3 3 0 013-3 3 3 0 012.14.89 3 3 0 01.88 2.13v5.9a2.42 2.42 0 104.83 0zm2.72 8.45a2.41 2.41 0 00-2.41-2.41h-24.5a2.42 2.42 0 000 4.83h24.46a2.41 2.41 0 002.41-2.42z"
+              fill="#e83c3d"
+            ></path>
+          </svg>
+        </div>
+        <div id="loginArea">
+          <form
+            id="loginForm"
+            name="LoginForm"
+            method="POST"
+            onSubmit={prelogin}
+          >
+            <input
+              id="id"
+              className="inputText"
+              name="id"
+              type="text"
+              placeholder="아이디"
+              autoComplete="off"
+              onChange={handleChange}
+            />
+            <input
+              id="pw"
+              className="inputText"
+              name="pw"
+              type="password"
+              placeholder="비밀번호"
+              onChange={handleChange}
+            />
+            <button id="submitButton" type="submit">
+              로그인
+            </button>
+          </form>
+        </div>
+      </div>
+
+      <div id="mealBox">
+        <div id="mealContentWrap" className="">
+          <div id="mealTime">아침</div>
+          <div id="meal">
+            {mealInfo.isError ? "급식 정보가 없습니다" : mealInfo.breakfast}
+          </div>
+        </div>
+        <div id="mealContentWrap" className="">
+          <div id="mealTime">점심</div>
+          <div id="meal">
+            {mealInfo.isError ? "급식 정보가 없습니다" : mealInfo.lunch}
+          </div>
+        </div>
+        <div id="mealContentWrap" className="selectedMealContentWrap">
+          <div id="mealTime">저녁</div>
+          <div id="meal">
+            {mealInfo.isError ? "급식 정보가 없습니다" : mealInfo.dinner}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
