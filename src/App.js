ff090e6a9665aac2e09304b719fa5abb99609890
jominjun <mj3029327@naver.com>
2020-09-30 16:52:11 +0900

    로그인 Form 완성

--- src/App.js
+++ src/App.js
@@ -1,44 +1,93 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import LoginForm from "./LoginForm";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import "./App.css";
 
-class App extends Component {
-  state = {
+const App = () => {
+  const [status, setStatus] = useState({
     isLogin: false,
+    isLoading: false,
+    isError: false,
+    errorStatusCode: "",
+  });
+
+  const [userInfo, setUserInfo] = useState({
+    id: "",
+    pw: "",
     accessToken: "",
     refreshToken: "",
+  });
+
+  const handleChange = (event) => {
+    setUserInfo({ ...userInfo, [event.target.name]: event.target.value });
   };
 
-  login = (data) => {
-    axios
+  const prelogin = () => {
+    // 로딩 중이 아니며 로그인하지 않았다면
+    if (!status.isLoading && !status.isLogin && !status.isError) {
+      setStatus({ ...status, isLoading: true });
+      login();
+    }
+  };
+
+  const login = async () => {
+    const data = await axios
       .post("https://api.dimigo.in/auth/", {
-        id: data.id,
-        password: data.password,
+        id: userInfo.id,
+        password: userInfo.pw,
       })
       .then((response) => {
-        this.setState({
-          isLogin: true,
-          accessToken: response.data.token,
-          refreshToken: response.data.refreshToken,
-        });
+        console.log("로그인 성공");
+        setStatus({ ...status, isLogin: true, isLoading: false });
       })
       .catch((error) => {
-        console.log(error);
+        setStatus({
+          ...status,
+          isError: true,
+          errorStatusCode: error.response.status,
+          isLoading: false,
+        });
       });
   };
 
-  render() {
-    if (this.state.isLogin == false) {
-      return (
-        <>
-          <LoginForm onCreate={this.login} />
-        </>
-      );
-    } else {
-      return <>로그인 되었습니다</>;
-    }
+  if (status.isLoading) {
+    return (
+      <>
+        <CircularProgress color="secondary" />
+      </>
+    );
   }
-}
+
+  if (status.isError) {
+    if (status.errorStatusCode === 404) {
+      return <>아이디 또는 패스워드가 일치하지 않습니다</>;
+    }
+    return <>Error: {status.errorStatusCode}</>;
+  }
+
+  if (status.isLogin) {
+    return <>로그인 함</>;
+  }
+
+  return (
+    <>
+      <form name="LoginForm" onSubmit={prelogin}>
+        <input
+          name="id"
+          type="text"
+          placeholder="아이디를 입력하세요"
+          onChange={handleChange}
+        />
+        <input
+          name="pw"
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+          onChange={handleChange}
+        />
+        <button type="submit">로그인</button>
+      </form>
+    </>
+  );
+};
 
 export default App;
